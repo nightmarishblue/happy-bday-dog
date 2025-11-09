51fb2eb9fbe27c8299d944a70149c19e30927e3c
blue <midnight@nightmarish.blue>
2025-11-09 14:29:41 +0000

    Add floaty effect to my signature

---
+++ scripts/floaty-text.js
@@ -0,0 +1,14 @@
+const DELAY_INCREMENT = 0.1;
+
+for (const el of document.getElementsByClassName("floaty-text")) {
+    /** @type {string} */
+    const text = el.dataset.text;
+    console.log(text)
+    el.innerHTML = text
+        .split('')
+        .map((char, i) => {
+            const displayChar = char === ' ' ? '&nbsp;' : char;
+            return `<span class="floaty-char" style="animation-delay:${i * DELAY_INCREMENT}s;">${displayChar}</span>`
+        })
+        .join('');
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 import './bgm.js';
 import './el-comandante.js';
 import './oneko.js';
+import './floaty-text.js'
